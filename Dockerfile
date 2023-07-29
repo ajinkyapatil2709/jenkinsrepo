d72b45a80daa303e720b00a3d940839043d1a329
Ajinkya Patil <ajinkyakpatil999@gmail.com>
2023-07-29 10:50:44 +0530

    itil

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM 192.168.0.10:5000/python
+FROM python
 RUN pip install flask
 COPY . .
-EXPOSE 7070
+EXPOSE 4000
 CMD python server.py
--- server.py
+++ server.py
@@ -1,10 +1,33 @@
 from flask import Flask
 
+# create flask app
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+courses = ['FCN',
+           ' COSA',
+            'SECURITY CONCEPTS',
+            'ITIM & DEVOPS',
+            'NDC',
+            'PKI',
+            'CYBER FORENSICS',
+           ' COMPLIANCE AUDIT', 
+           ' APPTITUDE and EFFECTIVE COMMUNICATION' ]
+name = [ 'Name : Ajinkya Patil' , 'Course : PG-DITISS', ' PRN : 230344223002', 'PHONE NUMBER : 8888407020']
+# add all the routes
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=7070, debug=True)
+@app.route("/", methods=["GET"])
+def root():
+    return "welcome to ITIL EXAM"
+
+@app.route("/modules", methods=["GET"])
+def get_list():
+    return courses
+
+@app.route("/me", methods=["GET"])
+def name():
+    return name
+
+
+
+# run the application
+app.run(host="0.0.0.0", port=4000, debug=True)
